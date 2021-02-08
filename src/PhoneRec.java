09751c0f5e39aad7d213a13cac9bae9f306e7784
almukanov <75900321+almukanov@users.noreply.github.com>
2021-02-08 20:18:38 +0500

    spravochnik

--- src/PhoneRec.java
+++ src/PhoneRec.java
@@ -1,2 +1,28 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class PhoneRec {
+    String name;
+    String phoneNumber;
+
+    HashMap<String, String> pr = new HashMap<>();
+
+    public void add(String name, String phoneNumber) {
+        pr.put(name, phoneNumber);
+    }
+
+    public void get(String name) {
+        for(int i =0; i<pr.size();i++) {
+            System.out.println(pr.get(name));
+        }
+    }
 }
+class Search {
+
+        public static void main(String[] args) {
+            PhoneRec p1 = new PhoneRec();
+            p1.add("Ivanov", "8754512310");
+            p1.get("Ivanov");
+
+        }
+    }
